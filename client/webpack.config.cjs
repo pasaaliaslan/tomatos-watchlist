40a72deaa8c5d7e2d035aab5ecdc7c9c0fe9885d
pasaaliaslan <pasaali1603@gmail.com>
2022-02-24 22:17:13 -0500

    FIX: html issue2

--- client/webpack.config.cjs
+++ client/webpack.config.cjs
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: path.join(__dirname, 'client', 'index.tsx'),
+    entry: path.join(__dirname, 'index.tsx'),
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'bundle.js',
@@ -39,12 +39,12 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.join(__dirname, './public/index.html'),
+            template: path.join(__dirname, '../public/index.html'),
         }),
         new CleanWebpackPlugin(),
     ],
     devServer: {
-        static: path.join(__dirname, './public'),
+        static: path.join(__dirname),
         port: 3001,
         hot: 'only',
         compress: true,
