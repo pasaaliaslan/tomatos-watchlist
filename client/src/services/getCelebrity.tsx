417a59a9a0348816d0728788f46d8780e29d4088
pasaaliaslan <pasaali1603@gmail.com>
2022-02-25 00:33:13 -0500

    FIX: links1

--- client/src/services/getCelebrity.tsx
+++ client/src/services/getCelebrity.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default async function getCelebrity(name: string) {
     return await axios
-        .get(`http://tomatos-watchlist.herokuapp.com/getCelebrity/${name}`)
+        .get(`https://tomatos-watchlist.herokuapp.com/getCelebrity/${name}`)
         .then((response) => (response.status == 200 ? response.data : response.status))
         .catch(() => 'NOT FOUND');
 }
--- client/src/services/getMovie.tsx
+++ client/src/services/getMovie.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default async function getMovie(path: string) {
     return await axios
-        .get(`http://tomatos-watchlist.herokuapp.com/getMovie/${path}`)
+        .get(`https://tomatos-watchlist.herokuapp.com/getMovie/${path}`)
         .then((response) => (response.status == 200 ? response.data : response.status))
         .catch(() => 'NOT FOUND');
 }
