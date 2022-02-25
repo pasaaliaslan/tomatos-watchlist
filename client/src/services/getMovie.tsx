af56c6ff8cec92dc1c7cb53ecce65a14b6436059
pasaaliaslan <pasaali1603@gmail.com>
2022-02-25 00:30:42 -0500

    FIX: links

--- client/src/services/getMovie.tsx
+++ client/src/services/getMovie.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default async function getMovie(path: string) {
     return await axios
-        .get(`http://0.0.0.0:8000/getMovie/${path}`)
+        .get(`http://tomatos-watchlist.herokuapp.com/getMovie/${path}`)
         .then((response) => (response.status == 200 ? response.data : response.status))
         .catch(() => 'NOT FOUND');
 }
